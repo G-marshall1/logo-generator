9fedbca13089225300edc91f15dae47e2b49f4bd
Graham Marshall <gmarshalltech1@gmail.com>
2023-10-31 22:15:28 -0600

    Added Inquirer prompt with a .then and catch for error handling

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const inquirer = require('inquirer');
 const fs = require('fs')
 const Triangle = require('./lib/shapes')
 const Cirlce = require('./lib/shapes')
-const Square = require('./lib/shapes')
+const Square = require('./lib/shapes');
+const { userInfo } = require('os');
 
 function generateSVG(shapeType, text, textColor, shapeColor) {
 
@@ -33,3 +34,41 @@ function saveSVGFile(svgInfo) {
     fs.writeFileSync('logo.svg', svgInfo, 'utf-8');
     console.log('generated logo.svg');
 }
+
+inquirer
+.prompt([
+    {
+        type: 'input',
+        name: 'text',
+        massage: 'Please enter 3 charaters for the text',
+        validate: (input) => {
+            return input.length <= 3;
+        },
+    },
+    {
+        type:'input',
+        name: 'textColor',
+        message: 'Please enter a color or a hexadecimal number for the text',
+    },
+    {
+        type: 'input',
+        name: 'textColor',
+        message: 'What shape would you like?',
+        choices: ['Triangle', 'Circle', 'Square'],
+    },
+    {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'Please enter color or hexadecimal number for the shape'
+    },
+])
+.then((userInput) => {
+    const {shape, text, textColor, shapeColor } = userInput;
+
+    const svgInfo = generateSVG(shape, text, textColor, shapeColor);
+
+    saveSVGFile(svgInfo);
+})
+.catch((error) => {
+    console.error('THIS NOT THE SVG YOU ARE LOOKING FOR! ITS', error );
+});

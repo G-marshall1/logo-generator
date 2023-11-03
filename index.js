a55cba4a2022684c46ba047c77b05fde14e38957
Graham Marshall <gmarshalltech1@gmail.com>
2023-11-02 22:12:27 -0600

    fixed all bugs and issues. code is now running

--- index.js
+++ index.js
@@ -4,83 +4,65 @@ const { Triangle, Circle, Square } = require('./lib/shapes');
 
 
 function writeToFile(fileName, answers) {
-
+console.log(answers)
     let shapeO = "";
 
-    shaperO = '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
-
-    shapeO += "<g>";
-
-    shapeO += `${answers.shape}`;
+    shapeO = '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
 
 
     let shapeImage;
     if (answers.shape === "Triangle") {
         shapeImage = new Triangle();
-        shapeO += `<polygon points="200,10 250,190 160,210" fill="${answers.shapeBackgroundColor}" />`;
+        shapeO += `<polygon points="150, 18 244, 182 56, 182" fill="${answers.shapeBackgroundColor}" />`;
     } else if (answers.shape === "Circle") {
         shapeImage = new Circle();
-        shapeO += `<circle cx="50" cy="50" r="40" fill="${answers.shapeBackgroundColor}" />`;
+        shapeO += `<circle cx="150" cy="125" r="80" fill="${answers.shapeBackgroundColor}" />`;
     } else if (answers.shape === "Square") {
         shapeImage = new Square();
         shapeO += `<rect width="100" height="100" fill="${answers.shapeBackgroundColor}" />`;
     }
 
-    shapeO =+ `<text x="150" y="100" font-family="Verdana" font-size="20" fill="${answers.textColor}">${answers.text}</text>`;
-
-    shapeO += "</g>";
+    shapeO += `<text x="150" y="150" font-family="Verdana" font-size="60" text-anchor="middle" fill="${answers.textColor}">${answers.text}</text>`;
 
 
     shapeO += "</svg>";
 
-    fs.writeFile(fileName, shapeO, function (err) {
-        if (err) {
-            return console.log(err);
-        }
-
-        console.log("Success!");
+    fs.writeFile(fileName, shapeO, (err) => {
+        err ? console.log(err) : console.log("Generated logo.svg");
+      });
     }
-
-    );
-
-}
 
 function promptMe() {
     inquirer.prompt([
         {
             type: "input",
-            message: "What is your name?",
-            name: "name"
+            message: "What 3 letters would you like to use for your logo text?",
+            name: "text",
         },
         {
             type: "list",
             message: "What shape would you like to use?",
             name: "shape",
-            choices: ["Triangle", "Circle", "Square"]
+            choices: ["Triangle", "Circle", "Square"],
         },
         {
             type: "input",
-            message: "What is your favorite color?",
-            name: "shapeBackgroundColor"
+            message: "What is your favorite color? (Enter color keyword OR a hexadecimal number)",
+            name: "shapeBackgroundColor",
         },
         {
             type: "input",
-            message: "What is your favorite color for the text?",
-            name: "textColor"
+            message: "What is your favorite color for the text? (Enter color keyword OR a hexadecimal number)",
+            name: "textColor",
         },
-        {
-            type: "input",
-            message: "What would you like to say?",
-            name: "text"
-        },
-        {
-            type: "input",
-            message: "What is the name of your file?",
-            name: "fileName"
+    ]).then((answers) => {
+        if (answers.text.length > 3) {
+            console.log("Please enter only 3 letters");
+            promptMe();
+        }else {
+            writeToFile("logo.svg", answers)
         }
-    ]).then(function (answers) {
-        writeToFile(answers.fileName, answers);
-    })
+    });
 }
 
 promptMe();
